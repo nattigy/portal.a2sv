333c44d73d0120ed77207a971d27b837279b5e3e
natnaelawel <natnael.awel@gmail.com>
2022-07-13 15:29:10 +0300

    fix(web): update notfound page

--- a2sv_portal_web/components/common/Notfound.tsx
+++ a2sv_portal_web/components/common/Notfound.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import { Box, Button, Typography } from '@mui/material'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
-type Props = {}
+type Props = {
+    title?: string,
+    description?: string,
+    action?: () => void,
+    actionTitle?: string,
+}
 
 const Notfound = (props: Props) => {
+    const router = useRouter()
+    const {
+        title = "Whoops!!",
+        description = "Looks like you are not connected to internet",
+        action = () => { router.reload() },
+        actionTitle = "Reload"
+    } = props
+
     return (
         <div>
             <Box sx={{
@@ -17,9 +31,9 @@ const Notfound = (props: Props) => {
                         <Image objectFit="cover" height={300} width={800} src="/assets/imgs/404.svg" alt="not-found" />
                     </Box>
                     <Box display="flex" rowGap={2} flexDirection="column" justifyItems="center" alignItems="center">
-                        <Typography fontWeight="bold" variant="h3">Whoops!!</Typography>
-                        <Typography variant="h6" width={200} align="center">Looks like you are not connected to internet</Typography>
-                        <Button color="primary" variant="contained">Reload</Button>
+                        <Typography fontWeight="bold" variant="h3">{title}</Typography>
+                        <Typography variant="h6" width={200} align="center">{description}</Typography>
+                        <Button color="primary" variant="contained" onClick={action}>{actionTitle}</Button>
                     </Box>
                 </Box>
             </Box>
--- a2sv_portal_web/pages/404.tsx
+++ a2sv_portal_web/pages/404.tsx
@@ -1,12 +1,14 @@
 import { Button, Grid, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import React from 'react'
 import Notfound from '../components/common/Notfound'
 
 type Props = {}
 
 function NotFoundPage({ }: Props) {
+
     return (
         <div>
             <Notfound />
