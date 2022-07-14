bb1a7a2582d60a2d1c40838ec3a28ca5a793a88d
Mintesnot Bezabhi <61611671+Mintaa911@users.noreply.github.com>
2022-07-14 06:47:18 +0300

    feat(web): implement authentication layout UI
    * feat(web): implement authentication layout UI
    
    * fix(web): fix bug inside package.json
    
    * fix(web): fix bug in package-lock.json
    
    * fix(web): remove unused imports

---
+++ a2sv_portal_web/components/auth/Rectangle.tsx
@@ -0,0 +1,22 @@
+import { Box, SxProps } from "@mui/material";
+
+type Props = {
+	color: string;
+	padx: number;
+	pady: number;
+	margin: number;
+	borderRadius: number;
+};
+
+const Rectangel = (myProps: Props) => {
+	const props: SxProps = {
+		bgcolor: myProps.color,
+		px: myProps.padx,
+		py: myProps.pady,
+		m: myProps.margin,
+		borderRadius: myProps.borderRadius,
+	};
+	return <Box sx={{ ...props }}></Box>;
+};
+
+export default Rectangel;
---
+++ a2sv_portal_web/pages/auth/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Layout from "../../components/auth/Layout";
+
+const AuthPage: NextPage = () => {
+	return (
+		<Layout>
+			<></>
+		</Layout>
+	);
+};
+
+export default AuthPage;
