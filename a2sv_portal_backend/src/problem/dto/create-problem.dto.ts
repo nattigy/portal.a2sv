a1e1b898ea21a529afbf914a1834b315a2aacba9
aben-bel <abenezer.sbelay@gmail.com>
2022-07-18 08:43:44 +0300

    [Backend] update schema and create DTO and Entity

--- a2sv_portal_backend/src/problem/dto/create-problem.dto.ts
+++ a2sv_portal_backend/src/problem/dto/create-problem.dto.ts
@@ -1,1 +1,29 @@
-export class CreateProblemDto {}
+import { IsEmpty, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateProblemDto {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  problem_title: String;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  platform: String;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  link: String;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  difficulty: String;
+
+  @IsString()
+  @IsEmpty()
+  @ApiProperty()
+  type: String;
+}
--- a2sv_portal_backend/src/problem/entities/problem.entity.ts
+++ a2sv_portal_backend/src/problem/entities/problem.entity.ts
@@ -1,1 +1,31 @@
-export class Problem {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ProblemEntity {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  problem_title: String;
+
+  @ApiProperty()
+  platform: String;
+
+  @ApiProperty()
+  link: String;
+
+  @ApiProperty()
+  difficulty: String;
+
+  @ApiProperty()
+  type: String;
+
+  constructor(partial: Partial<ProblemEntity>) {
+    Object.assign(this, partial);
+  }
+}
