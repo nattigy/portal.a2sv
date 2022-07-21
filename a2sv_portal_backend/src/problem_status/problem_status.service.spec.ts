bd568a53c4dca24e39552950ca61914b7f4f84d6
aben-bel <abenezer.sbelay@gmail.com>
2022-07-21 08:22:30 +0300

    [Backend] test for problem_status service

--- a2sv_portal_backend/src/problem_status/problem_status.service.spec.ts
+++ a2sv_portal_backend/src/problem_status/problem_status.service.spec.ts
@@ -1,7 +1,10 @@
 import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import exp from 'constants';
 import { PrismaService } from '../prisma/prisma.service';
 import { prismaMock } from '../singleton';
+import { CreateProblemStatusDto } from './dto/create-problem_status.dto';
+import { UpdateProblemStatusDto } from './dto/update-problem_status.dto';
 import { ProblemStatusService } from './problem_status.service';
 
 describe('ProblemStatusService', () => {
@@ -55,29 +58,126 @@ describe('ProblemStatusService', () => {
         });
       });
 
-      describe('otherwise', ()=>{
-        it('should throw the NotFoundException', async ()=>{
+      describe('otherwise', () => {
+        it('should throw the NotFoundException', async () => {
           prisma.problemStatus.findUnique.mockReturnValue(undefined);
 
-          try{
+          try {
             await service.findOne(1);
-          }catch(e){
+          } catch (e) {
             expect(e).toBeInstanceOf(NotFoundException);
             expect(e.message).toEqual(`ProblemStatus #1 not found`);
           }
-        })
-      })
+        });
+      });
     });
 
-    describe('update', ()=>{
-      describe('when problem with id exsit', ()=>{
-        it('should update the problem', async ()=>{
+    describe('update', () => {
+      describe('when problem with id exsit', () => {
+        it('should update the problem', async () => {
           const psId = 1;
           const expectedProblemStatus = {
-            
+            id: 0,
+            user_id: 0,
+            problem_id: 0,
+            status: 'Solved',
+            github_link: 'string',
+            number_of_tries: 0,
+            time_taken: 0,
+            date: new Date('2022-07-19T09:07:14.294Z'),
+            language_code: 'string',
+            solution_code: 'string',
+          };
+
+          prisma.problemStatus.update.mockReturnValue(expectedProblemStatus);
+          prisma.problemStatus.findUnique.mockReturnValue(
+            expectedProblemStatus,
+          );
+
+          const problem = await service.update(
+            psId,
+            new UpdateProblemStatusDto({ status: 'Solved' }),
+          );
+          expect(problem).toEqual(expectedProblemStatus);
+        });
+      });
+
+      describe('otherwise', () => {
+        it('should throw the NotFoundException', async () => {
+          prisma.problemStatus.findUnique.mockReturnValue(undefined);
+
+          try {
+            await service.update(1, {});
+          } catch (err) {
+            expect(err).toBeInstanceOf(NotFoundException);
+            expect(err.message).toEqual(`ProblemStatus #1 not found`);
           }
-        })
-      })
-    })
+        });
+      });
+    });
+
+    describe('create', () => {
+      describe('when given needed fields', () => {
+        it('should create problem', async () => {
+          const expectedProblemStatus = {
+            id: 0,
+            user_id: 0,
+            problem_id: 0,
+            status: 'Solved',
+            github_link: 'string',
+            number_of_tries: 0,
+            time_taken: 0,
+            date: new Date('2022-07-19T09:07:14.294Z'),
+            language_code: 'string',
+            solution_code: 'string',
+          };
+
+          prisma.problemStatus.create.mockReturnValue(expectedProblemStatus);
+
+          const data: CreateProblemStatusDto = {
+            user_id: 0,
+            problem_id: 0,
+            status: 'Solved',
+            github_link: 'string',
+            number_of_tries: 0,
+            time_taken: 0,
+            date: new Date('2022-07-19T09:07:14.294Z'),
+            language_code: 'string',
+            solution_code: 'string',
+          };
+
+          const problemStatus = await service.create(data);
+          expect(problemStatus).toEqual(expectedProblemStatus);
+        });
+      });
+    });
+
+    describe('remove', () => {
+      describe('when problem with id exists', () => {
+        it('should remove problemStatus', async () => {
+          const psid = 1;
+          const expectedValue = { data: 'data' };
+
+          prisma.problemStatus.findUnique.mockReturnValue(expectedValue);
+          prisma.problemStatus.delete.mockReturnValue(expectedValue);
+
+          const removed = await service.remove(psid);
+          expect(removed).toEqual(expectedValue);
+        });
+      });
+
+      describe('otherwise', () => {
+        it('should throw the NotFoundException', async () => {
+          prisma.problemStatus.findUnique.mockReturnValue(undefined);
+
+          try {
+            await service.remove(1);
+          } catch (err) {
+            expect(err).toBeInstanceOf(NotFoundException);
+            expect(err.message).toEqual(`ProblemStatus #1 not found`);
+          }
+        });
+      });
+    });
   });
 });
