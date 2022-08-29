02fded88025cfdf1f60b06c55a8d6a4fd356d8f8
nattigy <nathnaelakale@gmail.com>
2022-08-29 10:48:50 +0300

    [mobile] added custom button and input field

--- a2sv_portal_mobile/lib/widgets/buttons/main-button.dart
+++ a2sv_portal_mobile/lib/widgets/buttons/main-button.dart
@@ -5,6 +5,6 @@ class MainButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ElevatedButton(onPressed: (){}, child: Text("Button"));
   }
 }
--- a2sv_portal_mobile/lib/widgets/inputs/main-input-field.dart
+++ a2sv_portal_mobile/lib/widgets/inputs/main-input-field.dart
@@ -5,6 +5,6 @@ class MainInputField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextField();
   }
 }
