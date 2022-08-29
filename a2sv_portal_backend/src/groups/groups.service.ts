45a379cd44bb2c5730eb49b0ae519d2a202b4700
Miruts Hadush <miruts.hadush@gmail.com>
2022-08-29 21:08:09 +0300

    implement group model

--- a2sv_portal_backend/src/groups/groups.service.ts
+++ a2sv_portal_backend/src/groups/groups.service.ts
@@ -2,17 +2,45 @@ import { Injectable, NotFoundException } from '@nestjs/common'
 import { PrismaService } from 'src/prisma.service'
 import { CreateGroupInput } from './dto/create-group.input'
 import { Group } from '@prisma/client'
+import { UpdateGroupInput } from './dto/update-group.input'
 
 @Injectable()
 export class GroupsService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  findOneById(id: number): Promise<Group> {
-    const group = this.prismaService.group.findUnique({ where: { id: id } })
+  async createGroup(createGroupInput: CreateGroupInput): Promise<Group> {
+    return this.prismaService.group.create({
+      data: createGroupInput,
+    })
+  }
+
+  async getGroupById(id: number): Promise<Group> {
+    const group = await this.prismaService.group.findUnique({
+      where: { id },
+    })
+    if (!group) {
+      throw new NotFoundException(`Group with id ${id} not found`)
+    }
     return group
   }
 
-  create(group: CreateGroupInput): Promise<Group> {
-    return this.prismaService.group.create({ data: group })
+  async getGroups(): Promise<Group[]> {
+    return this.prismaService.group.findMany()
+  }
+
+  async updateGroup(
+    id: number,
+    updateGroupInput: UpdateGroupInput,
+  ): Promise<Group> {
+    return this.prismaService.group.update({
+      where: { id },
+      data: updateGroupInput,
+    })
+  }
+
+  async deleteGroup(id: number): Promise<Group> {
+    return this.prismaService.group.delete({
+      where: { id },
+    })
   }
 }
