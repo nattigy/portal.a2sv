331705037d29dcbabb0630af6b0d1118168081f0
Miruts Hadush <miruts.hadush@gmail.com>
2022-08-29 17:32:46 +0300

    implement role model, add heroku deployment configs

--- a2sv_portal_backend/src/app.module.ts
+++ a2sv_portal_backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { GraphQLModule } from '@nestjs/graphql'
 import { join } from 'path'
 import { AppService } from './app.service'
 import { GroupsModule } from './groups/groups.module'
+import { RolesModule } from './roles/roles.module'
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { GroupsModule } from './groups/groups.module'
       sortSchema: true,
     }),
     GroupsModule,
+    RolesModule,
   ],
   providers: [AppService],
 })
