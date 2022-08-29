89f89d1671f67ddaf4f8597b018c5f396cc177e8
Miruts Hadush <miruts.hadush@gmail.com>
2022-08-29 21:08:09 +0300

    implement group model

--- a2sv_portal_backend/src/groups/groups.resolver.ts
+++ a2sv_portal_backend/src/groups/groups.resolver.ts
@@ -7,6 +7,7 @@ import {
   Resolver,
 } from '@nestjs/graphql'
 import { CreateGroupInput } from './dto/create-group.input'
+import { UpdateGroupInput } from './dto/update-group.input'
 import { Group } from './entities/group.entity'
 import { GroupsService } from './groups.service'
 
@@ -14,15 +15,31 @@ import { GroupsService } from './groups.service'
 export class GroupsResolver {
   constructor(private readonly groupsService: GroupsService) {}
 
-  @Query(() => Group, { nullable: true })
-  async group(@Args('id', { type: () => Int }) id: number): Promise<Group> {
-    return this.groupsService.findOneById(id)
+  @Mutation(() => Group)
+  createGroup(@Args('createGroupInput') createGroupInput: CreateGroupInput) {
+    return this.groupsService.createGroup(createGroupInput)
   }
 
-  @Mutation((type) => Group, { nullable: true })
-  async create(
-    @Args('createGroupInput') args: CreateGroupInput,
-  ): Promise<Group> {
-    return this.groupsService.create(args)
+  @Query(() => [Group])
+  groups() {
+    return this.groupsService.getGroups()
+  }
+
+  @Query(() => Group)
+  group(@Args('id', { type: () => Int }) id: number) {
+    return this.groupsService.getGroupById(id)
+  }
+
+  @Mutation(() => Group)
+  updateGroup(
+    @Args('id', { type: () => Int }) id: number,
+    @Args('updateGroupInput') updateGroupInput: UpdateGroupInput,
+  ) {
+    return this.groupsService.updateGroup(id, updateGroupInput)
+  }
+
+  @Mutation(() => Group)
+  deleteGroup(@Args('id', { type: () => Int }) id: number) {
+    return this.groupsService.deleteGroup(id)
   }
 }
