b17076fb450147b55d4c6ccf2b4c2fd8eddb2c38
Daniel-Araya-Abera <danielaraya2191@gmail.com>
2022-09-02 11:11:08 +0300

    [Mobile] add more optional fields to CustomInputField

--- a2sv_portal_mobile/lib/widgets/inputs/main-input-field.dart
+++ a2sv_portal_mobile/lib/widgets/inputs/main-input-field.dart
@@ -1,18 +1,32 @@
+import 'package:a2sv_portal_mobile/utils/custom_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:formz/formz.dart';
 
 class MainInputField extends StatelessWidget {
   final String placeHolder;
+  final String? customLabelText;
   final Color? color;
+  final Color? iconColor;
+  final Color? fillColor;
   final IconData? iconData;
-  final Function onChanged;
+  final ValueChanged<String>? onChanged;
+  final FormzInput? formzInput;
+  final String? customErrorText;
+  final bool obscureText;
 
-  const MainInputField(
-      {Key? key,
-      required this.placeHolder,
-      required this.color,
-      this.iconData,
-      required this.onChanged})
-      : super(key: key);
+  const MainInputField({
+    Key? key,
+    required this.placeHolder,
+    required this.color,
+    this.fillColor,
+    this.iconData,
+    required this.onChanged,
+    this.formzInput,
+    this.customLabelText,
+    this.customErrorText,
+    this.obscureText = false,
+    this.iconColor,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,20 +39,30 @@ class MainInputField extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
       child: TextFormField(
-        onChanged: (value) => onChanged(),
+        onChanged: onChanged,
         decoration: InputDecoration(
-          prefixIcon: Icon(iconData),
+          prefixIcon: Icon(
+            iconData,
+            color: iconColor,
+          ),
           focusedBorder: customOutlineInputBorder,
           filled: true,
-
           fillColor: color,
           focusColor: color,
           // fillColor: Color.fromRGBO(250, 251, 255, 1),
           border: customOutlineInputBorder,
-          hintStyle: const TextStyle(
-              color: Color.fromRGBO(164, 164, 164, 1), fontSize: 12),
+          hintStyle: TextStyle(
+            // color: Color.fromRGBO(164, 164, 164, 1),
+            color: CustomColors.veryLightTextColor,
+            fontSize: 12,
+          ),
           hintText: placeHolder,
+          labelText: customLabelText,
+          errorText: formzInput == null
+              ? null
+              : (formzInput!.invalid ? customErrorText : null),
         ),
+        obscureText: obscureText,
       ),
     );
   }
