e79312a2bc3d069c30a63660ae9d88b238f329ab
Miruts Hadush <miruts.hadush@gmail.com>
2022-09-02 14:37:10 +0300

    setup Apollo Playground on production

--- a2sv_portal_backend/src/app.module.ts
+++ a2sv_portal_backend/src/app.module.ts
@@ -1,6 +1,6 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
-import { GraphQLModule } from '@nestjs/graphql'
+import { GraphQLISODateTime, GraphQLModule } from '@nestjs/graphql'
 import { join } from 'path'
 import { AppService } from './app.service'
 import { GroupsModule } from './groups/groups.module'
@@ -8,15 +8,24 @@ import { RolesModule } from './roles/roles.module'
 import { UserModule } from './user/user.module'
 import { PrismaModule } from './prisma/prisma.module'
 import { SeasonModule } from './season/season.module'
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
+import { ConfigModule } from '@nestjs/config'
+import { AppResolver } from './app.resolver'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
-      introspection: true,
-      playground: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
+      subscriptions: {
+        'graphql-ws': true,
+        'subscriptions-transport-ws': true,
+      },
+      resolvers: { DateTime: GraphQLISODateTime },
     }),
     GroupsModule,
     RolesModule,
@@ -24,6 +33,6 @@ import { SeasonModule } from './season/season.module'
     PrismaModule,
     SeasonModule,
   ],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
