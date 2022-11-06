abba4530697ee93213de7e51f814bd2d1a8b5cec
AmirMustefa <amirmustefa707@gmail.com>
2022-11-06 08:20:49 +0300

    Add season topic docmentation

--- a2sv_portal_backend/src/problem/problem.resolver.ts
+++ a2sv_portal_backend/src/problem/problem.resolver.ts
@@ -13,7 +13,7 @@ import { CreateProblemInput } from './dto/create-problem.input'
 import { UpdateProblemInput } from './dto/update-problem.input'
 import { Tag } from '@prisma/client'
 import { Roles } from 'src/auth/auth.decorator'
-import {SeasonTopicProblem} from "../season-topic-problem/entities/season-topic-problem.entity";
+import { SeasonTopicProblem } from '../season-topic-problem/entities/season-topic-problem.entity'
 
 @Resolver(() => Problem)
 export class ProblemResolver {
@@ -66,7 +66,7 @@ export class ProblemResolver {
     name: 'problem',
     description: 'Find a unique problem by id',
   })
-  async problem(@Args('id', { type: () => Int }) id: string): Promise<Problem> {
+  async problem(@Args('id') id: string): Promise<Problem> {
     return await this.problemService.findOne(id)
   }
 
--- a2sv_portal_backend/src/season-topic/season-topic.service.ts
+++ a2sv_portal_backend/src/season-topic/season-topic.service.ts
@@ -1,104 +1,110 @@
-import {Injectable} from '@nestjs/common';
-import {CreateSeasonTopicInput} from './dto/create-season-topic.input';
-import {UpdateSeasonTopicInput} from './dto/update-season-topic.input';
-import {PrismaService} from "../prisma.service";
-import {Field, InputType} from "@nestjs/graphql";
+import { Injectable } from '@nestjs/common'
+import { CreateSeasonTopicInput } from './dto/create-season-topic.input'
+import { UpdateSeasonTopicInput } from './dto/update-season-topic.input'
+import { PrismaService } from '../prisma.service'
+import { Field, InputType, Int } from '@nestjs/graphql'
 
 @InputType()
 export class SeasonTopicFilter {
-    skip?: number
-    take?: number
-    seasonId?: string
-    topicId?: string
+  @Field(() => Int, { nullable: true })
+  skip?: number
+  @Field(() => Int, { nullable: true })
+  take?: number
+  @Field({ nullable: true })
+  seasonId?: string
+  @Field({ nullable: true })
+  topicId?: string
 }
 
 @InputType()
 export class SeasonTopicId {
-    @Field()
-    seasonId: string
-    @Field()
-    topicId: string
+  @Field()
+  seasonId: string
+  @Field()
+  topicId: string
 }
 
 @Injectable()
 export class SeasonTopicService {
-    constructor(private readonly prismaService: PrismaService) {
-    }
+  constructor(private readonly prismaService: PrismaService) {}
 
-    create(createSeasonTopicInput: CreateSeasonTopicInput) {
-        return this.prismaService.seasonTopic.create({
-            data: createSeasonTopicInput, include: {
-                topic: true,
-                season: true,
-                problems: {
-                    include: {
-                        problem: true
-                    }
-                }
-            }
-        })
-    }
+  create(createSeasonTopicInput: CreateSeasonTopicInput) {
+    return this.prismaService.seasonTopic.create({
+      data: createSeasonTopicInput,
+      include: {
+        topic: true,
+        season: true,
+        problems: {
+          include: {
+            problem: true,
+          },
+        },
+      },
+    })
+  }
 
-    findAll({take, skip, topicId, seasonId}: SeasonTopicFilter) {
-        return this.prismaService.seasonTopic.findMany({
-            take,
-            skip,
-            where: {
-                topicId,
-                seasonId
-            }
-        })
-    }
+  findAll({ take, skip, topicId, seasonId }: SeasonTopicFilter) {
+    return this.prismaService.seasonTopic.findMany({
+      take,
+      skip,
+      where: {
+        topicId,
+        seasonId,
+      },
+      include: {
+        season: true,
+        topic: true,
+      },
+    })
+  }
 
-    findOne({seasonId, topicId}: SeasonTopicId) {
-        return this.prismaService.seasonTopic.findUnique({
-            where: {
-                seasonId_topicId: {
-                    seasonId,
-                    topicId
-                }
-            },
-            include: {
-                season: true,
-                topic: true,
-                problems: {
-                    include: {
-                        problem: true
-                    }
-                }
-            }
-        })
-    }
+  findOne({ seasonId, topicId }: SeasonTopicId) {
+    return this.prismaService.seasonTopic.findUnique({
+      where: {
+        seasonId_topicId: {
+          seasonId,
+          topicId,
+        },
+      },
+      include: {
+        season: true,
+        topic: true,
+        problems: {
+          include: {
+            problem: true,
+          },
+        },
+      },
+    })
+  }
 
-    update({seasonId, topicId, problems, ...updates}: UpdateSeasonTopicInput) {
-        return this.prismaService.seasonTopic.update({
-                where: {
-                    seasonId_topicId: {
-                        seasonId,
-                        topicId
-                    }
-                },
-                data: {
-                    ...updates,
-                    problems: {
-                        createMany: {
-                            data: problems
-                        }
-                    }
-                }
-            }
-        )
+  update({ seasonId, topicId, problems, ...updates }: UpdateSeasonTopicInput) {
+    return this.prismaService.seasonTopic.update({
+      where: {
+        seasonId_topicId: {
+          seasonId,
+          topicId,
+        },
+      },
+      data: {
+        ...updates,
+        problems: {
+          createMany: {
+            data: problems,
+          },
+        },
+      },
+    })
+  }
 
-    }
-
-    remove({seasonId, topicId}: SeasonTopicId) {
-        return this.prismaService.seasonTopic.delete({
-            where: {
-                seasonId_topicId: {
-                    seasonId,
-                    topicId
-                }
-            }
-        })
-    }
+  remove({ seasonId, topicId }: SeasonTopicId) {
+    return this.prismaService.seasonTopic.delete({
+      where: {
+        seasonId_topicId: {
+          seasonId,
+          topicId,
+        },
+      },
+    })
+  }
 }
