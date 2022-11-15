9337c58f18f14ee2feac5f5344d04874bf8b35d0
nattigy <nathnaelakale@gmail.com>
2022-11-15 17:39:54 +0300

    fix:seasons filter option adjested

--- a2sv_portal_backend/prisma/seeds/users.ts
+++ a2sv_portal_backend/prisma/seeds/users.ts
@@ -8,6 +8,18 @@ export const users =[
         "status": Status.ACTIVE
     },
     {
+        "email": "nati@a2sv.org",
+        "password":  '$2b$10$meWlkGCFEHkP/serox2RGuOr3LQJBnPLweHt3ZmL0S0yBbSbg9Tnm',
+        "role": RoleEnum.HEAD_OF_EDUCATION,
+        "status": Status.ACTIVE
+    },
+    {
+        "email": "emre@a2sv.org",
+        "password":  '$2b$10$meWlkGCFEHkP/serox2RGuOr3LQJBnPLweHt3ZmL0S0yBbSbg9Tnm',
+        "role": RoleEnum.HEAD_OF_ACADEMY,
+        "status": Status.ACTIVE
+    },
+    {
         "email": "beza@a2sv.org",
         "password":  '$2b$10$meWlkGCFEHkP/serox2RGuOr3LQJBnPLweHt3ZmL0S0yBbSbg9Tnm',
         "role": RoleEnum.STUDENT,
