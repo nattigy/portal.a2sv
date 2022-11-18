ce7ded9d745ded9a32200a770a51dccfe65b33c7
nattigy <nathnaelakale@gmail.com>
2022-11-18 16:31:13 +0300

    fix: student stat get users problem fixed

--- a2sv_portal_backend/src/group/groups.service.ts
+++ a2sv_portal_backend/src/group/groups.service.ts
@@ -39,7 +39,11 @@ export class GroupsService {
               include: {
                 problems: {
                   include: {
-                    problem: true,
+                    problem: {
+                      include: {
+                        users: true
+                      }
+                    },
                   },
                 },
               },
