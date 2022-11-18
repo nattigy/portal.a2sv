bac4228387578a11ef449155033b66ebfc0e4ff0
nattigy <nathnaelakale@gmail.com>
2022-11-18 14:53:58 +0300

    fix: merge problems with user stat

--- a2sv_portal_backend/src/common/page/pagination-info.input.ts
+++ a2sv_portal_backend/src/common/page/pagination-info.input.ts
@@ -3,8 +3,8 @@ import { Field, InputType, Int } from '@nestjs/graphql'
 @InputType()
 export class PaginationInfoInput {
   @Field(() => Int, { nullable: true })
-  take: number
+  take?: number
 
   @Field(() => Int, { nullable: true })
-  skip: number
+  skip?: number
 }
