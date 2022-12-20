f2af97d17f9b888bcccdaa299e8dee3b547c2432
lensabillion <billionlensa2019@gmail.com>
2022-12-20 11:52:29 +0300

    Seeding completed

--- a2sv_portal_backend/prisma/seeds/contestData.ts
+++ a2sv_portal_backend/prisma/seeds/contestData.ts
@@ -1,21 +1,21 @@
-const contestData = [
-  {
-    'name': 'contest 1',
-    'link': 'link1',
-    'startTime': '2022-11-18T05:48:54.744Z',
-    'endTime': '2022-11-18T05:48:54.744Z',
-  },
-  {
-    'name': 'contest 2',
-    'link': 'link1',
-    'startTime': '2022-11-18T05:48:54.744Z',
-    'endTime': '2022-11-18T05:48:54.744Z',
-  },
-  {
-    'name': 'contest 3',
-    'link': 'link1',
-    'startTime': '2022-11-18T05:48:54.744Z',
-    'endTime': '2022-11-18T05:48:54.744Z',
-  },
-]
-export default contestData
+// const contestData = [
+//   {
+//     'name': 'contest 1',
+//     'link': 'link1',
+//     'startTime': '2022-11-18T05:48:54.744Z',
+//     'endTime': '2022-11-18T05:48:54.744Z',
+//   },
+//   {
+//     'name': 'contest 2',
+//     'link': 'link1',
+//     'startTime': '2022-11-18T05:48:54.744Z',
+//     'endTime': '2022-11-18T05:48:54.744Z',
+//   },
+//   {
+//     'name': 'contest 3',
+//     'link': 'link1',
+//     'startTime': '2022-11-18T05:48:54.744Z',
+//     'endTime': '2022-11-18T05:48:54.744Z',
+//   },
+// ]
+// export default contestData
