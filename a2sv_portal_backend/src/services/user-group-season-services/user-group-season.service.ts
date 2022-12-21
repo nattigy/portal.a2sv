30c73cc601ab2061c4d9840287d34db6cb2d6ef8
nattigy <nathnaelakale@gmail.com>
2022-12-21 18:03:11 +0300

    user group season stat inprogress

--- a2sv_portal_backend/src/services/user-group-season-services/user-group-season.service.ts
+++ a2sv_portal_backend/src/services/user-group-season-services/user-group-season.service.ts
@@ -5,6 +5,7 @@ import { FilterUserGroupSeasonInput } from '../../app/user-group-season/dto/filt
 import { UserGroupSeasonRepository } from '../../app/user-group-season/user-group-season.repository'
 import { UserGroupSeasonTopicService } from './user-group-season-topic.service'
 import { PrismaService } from '../../prisma/prisma.service'
+import { UserGroupSeason } from 'src/app/user-group-season/entities/user-group-season.entity'
 
 @Injectable()
 export class UserGroupSeasonService {
@@ -62,15 +63,70 @@ export class UserGroupSeasonService {
   }
 
   async userGroupSeasons(
-    { seasonId, userId }: FilterUserGroupSeasonInput,
+    { seasonId, userId, groupId }: FilterUserGroupSeasonInput,
     { take, skip }: PaginationInput = { take: 50, skip: 0 },
   ) {
     /// TODO generate multiple stat here
-    return this.userGroupSeasonRepository.findAll({
-      take,
-      skip,
-      where: { seasonId, userId },
+    const users = await this.prismaService.user.findMany({
+      where: { id: userId, groupId },
     })
+    const userSeasons = await this.userGroupSeasonRepository.findAll({
+      where: { userId, groupId, seasonId },
+    })
+    const statMap: { '[key]'?: UserGroupSeason } = {}
+    for (const userSeason of userSeasons) {
+      statMap[
+        `${userSeason.userId}${userSeason.groupId}${userSeason.seasonId}`
+        ] = userSeason
+    }
+    for (const user of users) {
+      for (const userSeason of userSeasons) {
+        const check = statMap[
+          `${user.id}${user.groupId}${userSeason.seasonId}`
+          ]
+        if (!check) {
+          statMap[
+            `${user.id}${user.groupId}${userSeason.seasonId}`
+            ] = {
+            userId: user.id,
+            groupId: user.groupId,
+            seasonId: userSeason.seasonId,
+            rank: 0,
+            totalSubmissions: 0,
+            totalAcceptedSubmissions: 0,
+            acceptanceRate: 0,
+            averageContestRating: 0,
+            totalContestsAttended: 0,
+            userGroupSeasonTopics: [],
+            userGroupSeasonContests: [],
+            user,
+          }
+        }
+      }
+    }
+    const usersTopics = (await this.userGroupSeasonTopicService.userGroupSeasonTopics({
+      userId, groupId, seasonId,
+    })).items
+    const userStats = []
+    for (const statMapKey in statMap) {
+      const stat = statMap[statMapKey]
+      const uTopics = usersTopics.filter(t => (
+        t.userId === stat.userId &&
+        t.groupId === stat.groupId &&
+        t.seasonId === stat.seasonId &&
+        t.topicId === stat.topicId
+      ))
+      const totalSubmissions = uTopics.map(t => t.totalSubmissions).reduce((a, b) => a + b, 0)
+      const totalAcceptedSubmissions = uTopics.map(t => t.totalAcceptedSubmissions).reduce((a, b) => a + b, 0)
+      const acceptanceRate = uTopics.map(t => t.totalSubmissions).reduce((a, b) => a + b, 0)
+      statMap[statMapKey] = {
+        ...statMap[statMapKey],
+        totalSubmissions,
+        totalAcceptedSubmissions,
+        acceptanceRate,
+      }
+    }
+    return userStats
   }
 
   async removeUserGroupSeason({ seasonId, groupId, userId }: UserGroupSeasonId) {
