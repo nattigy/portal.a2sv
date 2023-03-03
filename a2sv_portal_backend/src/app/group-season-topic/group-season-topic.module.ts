2e119c07405ea2875cfda651d6c02fce2a293ee6
nattigy <nathnaelakale@gmail.com>
2023-03-03 14:27:16 +0300

    added back groupSeasonTopic module

--- a2sv_portal_backend/src/app/group-season-topic/group-season-topic.module.ts
+++ a2sv_portal_backend/src/app/group-season-topic/group-season-topic.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { GroupSeasonTopicRepository } from './group-season-topic.repository'
 import { GroupSeasonTopicService } from './group-season-topic.service'
+import { GroupSeasonTopicResolver } from './group-season-topic.resolver'
 
 @Module({
-  providers: [GroupSeasonTopicRepository, GroupSeasonTopicService],
+  providers: [GroupSeasonTopicResolver, GroupSeasonTopicRepository, GroupSeasonTopicService],
   exports: [GroupSeasonTopicRepository, GroupSeasonTopicService],
 })
 export class GroupSeasonTopicModule {
